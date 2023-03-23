19792bc24c16c47ebfc82436cf4ff7f8977b2bb3
Kohei Yoshino <kohei.yoshino@gmail.com>
2023-03-22 20:56:20 -0400

    作品ギャラリーのフィルターを複数適用可能に

--- static/scripts/gallery.js
+++ static/scripts/gallery.js
@@ -1,22 +1,25 @@
 window.addEventListener(
   'DOMContentLoaded',
   () => {
-    const _categories = {};
-    const _years = {};
-    const _locations = {};
-    const $fragment = document.createDocumentFragment();
     const works = [];
+    const filters = {};
+    const awardMap = {};
+    const categoryMap = {};
+    const yearMap = {};
+    const locationMap = {};
+    const $fragment = document.createDocumentFragment();
     const $pageWrapper = document.querySelector('#main-pages');
     const $worksContainer = document.querySelector('#works-container');
     const $works = document.querySelector('#works-container > ul');
     const $title = document.querySelector('#works-container > h2');
     const $container = document.querySelector('#gallery-container');
     const $filters = document.querySelector('#filters');
-    const $filterGeneral = $filters.querySelector('#filter-general ul');
-    const $filterCategories = $filters.querySelector('#filter-categories ul');
-    const $filterYears = $filters.querySelector('#filter-years ul');
-    const $filterLocations = $filters.querySelector('#filter-locations ul');
-    const sorter = (a, b) => a[0] > b[0];
+    const $clearFilter = document.querySelector('#clear-filter');
+    const $filterAward = $filters.querySelector('#filter-award ul');
+    const $filterCategory = $filters.querySelector('#filter-category ul');
+    const $filterYear = $filters.querySelector('#filter-year ul');
+    const $filterLocation = $filters.querySelector('#filter-location ul');
+    const sorter = (a, b) => b[0].localeCompare(a[1]);
 
     // Randomize the works
     for (;;) {
@@ -25,6 +28,7 @@ window.addEventListener(
       }
 
       const index = Math.floor(Math.random() * $works.children.length);
+      // const index = 0; // Stop randomizing
       const child = $works.children[index];
 
       $fragment.appendChild(child);
@@ -34,15 +38,16 @@ window.addEventListener(
     $works.appendChild($fragment);
 
     works.forEach(($work) => {
-      const { categories, year, location } = $work.dataset;
+      const { award, categories, year, location } = $work.dataset;
       const $img = $work.querySelector('img');
 
       categories.split(' ').forEach((cat) => {
-        _categories[cat] = (_categories[cat] || 0) + 1;
+        categoryMap[cat] = (categoryMap[cat] || 0) + 1;
       });
 
-      _years[year] = (_years[year] || 0) + 1;
-      _locations[location] = (_locations[location] || 0) + 1;
+      awardMap[award] = (awardMap[award] || 0) + 1;
+      yearMap[year] = (yearMap[year] || 0) + 1;
+      locationMap[location] = (locationMap[location] || 0) + 1;
 
       // Lazy load images
       const observer = new IntersectionObserver((entries) => {
@@ -57,135 +62,129 @@ window.addEventListener(
       observer.observe($img);
     });
 
-    $filterGeneral.innerHTML = ` \
+    $filterAward.innerHTML = ` \
       <li role="none"> \
-        <a href="#" role="radio" aria-checked="true" data-category="すべて"> \
-          すべて (${works.length}) \
-        </a> \
+        <a href="#" role="radio" aria-checked="true" data-award="">指定なし (${works.length})</a> \
       </li> \
       <li role="none"> \
-        <a href="#" role="radio" aria-checked="false" data-award="*"> \
-          最優秀賞受賞作品 (${works.filter((work) => work.dataset.award).length}) \
+        <a href="#" role="radio" aria-checked="false" data-award="最優秀賞"> \
+          最優秀賞 (${works.filter((work) => work.dataset.award).length}) \
         </a> \
       </li> \
     `;
 
-    $filterCategories.innerHTML = Object.entries(_categories)
-      .sort(sorter)
-      .map(
-        ([category, count]) => ` \
-          <li role="none"> \
-            <a href="#" role="radio" aria-checked="false" data-category="${category}"> \
-              ${category} (${count}) \
-            </a> \
-          </li> \
-        `,
-      )
-      .join('');
+    $filterCategory.innerHTML = [
+      `
+        <li role="none"> \
+          <a href="#" role="radio" aria-checked="true" data-category="">すべて (${works.length})</a> \
+        </li> \
+      `,
+      ...Object.entries(categoryMap)
+        .sort(sorter)
+        .map(
+          ([category, count]) => ` \
+            <li role="none"> \
+              <a href="#" role="radio" aria-checked="false" data-category="${category}"> \
+                ${category} (${count}) \
+              </a> \
+            </li> \
+          `,
+        ),
+    ].join('');
 
-    $filterYears.innerHTML = Object.entries(_years)
-      .sort(sorter)
-      .map(
-        ([year, count]) => ` \
-          <li role="none"> \
-            <a href="#" role="radio" aria-checked="false" data-year="${year}"> \
-              ${year} (${count}) \
-            </a> \
-          </li> \
-        `,
-      )
-      .join('');
+    $filterYear.innerHTML = [
+      `
+        <li role="none"> \
+          <a href="#" role="radio" aria-checked="true" data-year="">すべて (${works.length})</a> \
+        </li> \
+      `,
+      ...Object.entries(yearMap)
+        .sort(sorter)
+        .map(
+          ([year, count]) => ` \
+            <li role="none"> \
+              <a href="#" role="radio" aria-checked="false" data-year="${year}"> \
+                ${year} (${count}) \
+              </a> \
+            </li> \
+          `,
+        ),
+    ].join('');
 
-    $filterLocations.innerHTML = Object.entries(_locations)
-      .sort(sorter)
-      .map(
-        ([location, count]) => ` \
-          <li role="none"> \
-            <a href="#" role="radio" aria-checked="false" data-location="${location}"> \
-              ${location} (${count}) \
-            </a> \
-          </li> \
-        `,
-      )
-      .join('');
-
-    /**
-     * Replace the current URL with the given search params.
-     * @param {Object} init Params.
-     */
-    const updateLocation = (init) => {
-      window.history.replaceState({}, '', `?${new URLSearchParams(init).toString()}`);
-    };
+    $filterLocation.innerHTML = [
+      `
+        <li role="none"> \
+          <a href="#" role="radio" aria-checked="true" data-location="">すべて (${works.length})</a> \
+        </li> \
+      `,
+      ...Object.entries(locationMap)
+        .sort(sorter)
+        .map(
+          ([location, count]) => ` \
+            <li role="none"> \
+              <a href="#" role="radio" aria-checked="false" data-location="${location}"> \
+                ${location} (${count}) \
+              </a> \
+            </li> \
+          `,
+        ),
+    ].join('');
 
     /**
      * Apply a filter to the work list, and update the view accordingly. Multiple filters like
      * year+location are not supported at this time.
-     * @param {Object} filter Filtering condition in the form of key-value pairs, where keys are
-     * `award`, `category`, `year` and `location`.
      */
-    const applyFilter = ({ award, category, year, location }) => {
-      if (award) {
-        works.forEach(($work) => {
-          // eslint-disable-next-line no-param-reassign
-          $work.hidden = award && $work.matches('[data-award=""]');
-        });
+    const applyFilter = () => {
+      const params = new URLSearchParams(window.location.search);
+      const award = params.get('award') || undefined;
+      const category = params.get('category') || undefined;
+      const year = params.get('year') || undefined;
+      const location = params.get('location') || undefined;
+      const titleText = [];
 
-        $filters.querySelectorAll('[role="radio"]').forEach(($radio) => {
-          $radio.setAttribute('aria-checked', !!$radio.dataset.award);
-        });
+      works.forEach(($work) => {
+        // eslint-disable-next-line no-param-reassign
+        $work.hidden =
+          (award ? !$work.matches(`[data-award="${award}"]`) : false) ||
+          (category ? !$work.matches(`[data-categories~="${category}"]`) : false) ||
+          (year ? !$work.matches(`[data-year="${year}"]`) : false) ||
+          (location ? !$work.matches(`[data-location="${location}"]`) : false);
+      });
 
-        $title.textContent = '最優秀賞受賞作品';
-        updateLocation({ award: '最優秀賞' });
-
-        return;
+      if (award in awardMap) {
+        titleText.push(award);
       }
 
-      if (category) {
-        works.forEach(($work) => {
-          // eslint-disable-next-line no-param-reassign
-          $work.hidden =
-            category !== 'すべて' && !$work.matches(`[data-categories~="${category}"]`);
-        });
+      $filters.querySelectorAll('[role="radio"][data-award]').forEach(($radio) => {
+        $radio.setAttribute('aria-checked', $radio.dataset.award === (award || ''));
+      });
 
-        $filters.querySelectorAll('[role="radio"]').forEach(($radio) => {
-          $radio.setAttribute('aria-checked', $radio.dataset.category === category);
-        });
-
-        $title.textContent = category;
-        updateLocation({ category });
-
-        return;
+      if (category in categoryMap) {
+        titleText.push(category);
       }
 
-      if (year) {
-        works.forEach(($work) => {
-          // eslint-disable-next-line no-param-reassign
-          $work.hidden = year !== 'すべて' && !$work.matches(`[data-year="${year}"]`);
-        });
+      $filters.querySelectorAll('[role="radio"][data-category]').forEach(($radio) => {
+        $radio.setAttribute('aria-checked', $radio.dataset.category === (category || ''));
+      });
 
-        $filters.querySelectorAll('[role="radio"]').forEach(($radio) => {
-          $radio.setAttribute('aria-checked', $radio.dataset.year === year);
-        });
-
-        $title.textContent = `${year} 年度`;
-        updateLocation({ year });
-
-        return;
+      if (year in yearMap) {
+        titleText.push(`${year} 年度`);
       }
 
-      if (location) {
-        works.forEach(($work) => {
-          // eslint-disable-next-line no-param-reassign
-          $work.hidden = location !== 'すべて' && !$work.matches(`[data-location="${location}"]`);
-        });
+      $filters.querySelectorAll('[role="radio"][data-year]').forEach(($radio) => {
+        $radio.setAttribute('aria-checked', $radio.dataset.year === (year || ''));
+      });
 
-        $filters.querySelectorAll('[role="radio"]').forEach(($radio) => {
-          $radio.setAttribute('aria-checked', $radio.dataset.location === location);
-        });
-
-        $title.textContent = location;
-        updateLocation({ location });
+      if (location in locationMap) {
+        titleText.push(location);
       }
+
+      $filters.querySelectorAll('[role="radio"][data-location]').forEach(($radio) => {
+        $radio.setAttribute('aria-checked', $radio.dataset.location === (location || ''));
+      });
+
+      $title.textContent = titleText.join(' / ');
+      $clearFilter.hidden = !award && !category && !year && !location;
     };
 
     $container.addEventListener('click', (event) => {
@@ -195,10 +194,22 @@ window.addEventListener(
         return;
       }
 
-      const { award, category, year, location } = $target.dataset;
-
       event.preventDefault();
-      applyFilter({ award, category, year, location });
+
+      Object.entries($target.dataset).forEach(([key, value]) => {
+        if (value) {
+          filters[key] = value;
+        } else {
+          delete filters[key];
+        }
+      });
+
+      const params = new URLSearchParams(JSON.parse(JSON.stringify(filters)));
+
+      params.sort();
+      window.history.replaceState({}, '', `?${params.toString()}`);
+      window.dispatchEvent(new PopStateEvent('popstate'));
+
       $filters.classList.remove('active');
 
       window.setTimeout(() => {
@@ -207,24 +218,17 @@ window.addEventListener(
       }, 100);
     });
 
-    window.addEventListener(
-      'load',
-      () => {
-        const params = new URLSearchParams(window.location.search);
-
-        applyFilter({
-          award: params.get('award'),
-          category: params.get('category'),
-          year: params.get('year'),
-          location: params.get('location'),
-        });
-      },
-      { once: true },
-    );
-
     document.querySelector('#filter-button').addEventListener('click', () => {
       $filters.classList.add('active');
       $filters.focus();
+    });
+
+    window.addEventListener('load', () => {
+      applyFilter();
+    });
+
+    window.addEventListener('popstate', () => {
+      applyFilter();
     });
   },
   { once: true },
